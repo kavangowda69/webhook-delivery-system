24dc15123aab6c902adba51c98948681d4760fa5
kavan gowda <kavangowda69@gmail.com>
2026-03-06 13:38:06 +0530

    update webhook schema to support webhook registration

--- api/models/webhook.py
+++ api/models/webhook.py
@@ -1,9 +1,13 @@
-from sqlalchemy import Column, Integer, String, Text
+from sqlalchemy import Column, Integer, String, Boolean
+from sqlalchemy.dialects.postgresql import ARRAY
 from api.database.database import Base
+
 
 class Webhook(Base):
     __tablename__ = "webhooks"
 
     id = Column(Integer, primary_key=True, index=True)
-    event_type = Column(String)
-    payload = Column(Text)
+    user_id = Column(String, index=True)
+    url = Column(String)
+    event_types = Column(ARRAY(String))
+    active = Column(Boolean, default=True)
