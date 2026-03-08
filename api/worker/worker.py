09f048ad913ebd684684ca9757fd6a6d8d9adda2
kavan gowda <kavangowda69@gmail.com>
2026-03-08 12:38:24 +0530

    feat: implement async webhook worker and Redis queue processing

--- api/worker/worker.py
+++ api/worker/worker.py
@@ -1,13 +1,23 @@
 import json
 import requests
 import redis
+import time
 
 from sqlalchemy.orm import Session
 from api.database.database import SessionLocal
 from api.models.delivery import Delivery
 from api.models.webhook import Webhook
 
+
 redis_client = redis.Redis(host="webhook_redis", port=6379, db=0)
+
+# Global rate limit (deliveries per second)
+RATE_LIMIT = 2
+
+# Track delivery count per second
+current_second = int(time.time())
+deliveries_this_second = 0
+
 
 def process_job(job_data):
 
@@ -58,15 +68,40 @@ def process_job(job_data):
 
 def start_worker():
 
+    global current_second
+    global deliveries_this_second
+
     print("Redis Worker started...")
 
     while True:
 
+        # Pull job from queue
         job = redis_client.brpop("webhook_queue")
 
         if job:
+
+            now = int(time.time())
+
+            # Reset counter if new second
+            if now != current_second:
+                current_second = now
+                deliveries_this_second = 0
+
+            # If rate limit reached, wait until next second
+            if deliveries_this_second >= RATE_LIMIT:
+                print("Rate limit reached — sleeping")
+                time.sleep(1)
+                current_second = int(time.time())
+                deliveries_this_second = 0
+
+                current_second = int(time.time())
+                deliveries_this_second = 0
+
             _, job_data = job
+
             process_job(job_data)
+
+            deliveries_this_second += 1
 
 
 if __name__ == "__main__":
